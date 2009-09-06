cf03f19b4f66dee7a0603f41035c75e32debe6e1
Takeshi Abe <tabe@fixedpoint.jp>
2009-09-06 15:57:01 +0900

    valid-isbn10? and valid-isbn13?

--- sitelib/errata/isbn.scm
+++ sitelib/errata/isbn.scm
@@ -1,15 +1,21 @@
 (library (errata isbn)
-  (export valid-isbn?
+  (export valid-isbn13?
+          valid-isbn10?
+          valid-isbn?
           isbn10->amazon)
   (import (rnrs)
           (only (core) format)
           (pregexp))
 
+  (define (valid-isbn13? str)
+    (pregexp-match "^[0-9]{13}$" str))
+
+  (define (valid-isbn10? str)
+    (pregexp-match "^[0-9]{9}[0-9X]$" str))
+
   (define (valid-isbn? str)
-    (cond ((pregexp-match "^[0-9]{9}[0-9X]$" str)
-           10)
-          ((pregexp-match "^[0-9]{13}$" str)
-           13)
+    (cond ((valid-isbn13? str) 13)
+          ((valid-isbn10? str) 10)
           (else #f)))
 
   (define (isbn10->amazon str)
--- tests/errata/isbn.scm
+++ tests/errata/isbn.scm
@@ -1,4 +1,11 @@
 #!/usr/bin/env ypsilon
 #!r6rs
 
-(import (errata isbn))
+(import (errata isbn) (xunit))
+
+(assert-boolean=? #f (valid-isbn? ""))
+(assert-= 10 (valid-isbn? "2222222222"))
+(assert-= 10 (valid-isbn? "475614084X"))
+(assert-= 13 (valid-isbn? "9784873114040"))
+
+(report)
