7474f87800acbe845b65f8ce1079094c4803144f
Takeshi Abe <tabe@fixedpoint.jp>
2009-09-15 02:17:26 +0900

    check digit

--- sitelib/errata/isbn.scm
+++ sitelib/errata/isbn.scm
@@ -7,11 +7,26 @@
           (only (core) format)
           (pregexp))
 
+  (define (char->digit c) (- (char->integer c) 48)) ; #\0
+
   (define (valid-isbn13? str)
-    (pregexp-match "^[0-9]{13}$" str))
+    (and (pregexp-match "^[0-9]{13}$" str)
+         (let ((digits (map char->digit (string->list str))))
+           (= (list-ref digits 12)
+              (mod (- 10 (mod (apply + (map * digits '(1 3 1 3 1 3 1 3 1 3 1 3 0))) 10)) 10)))))
 
   (define (valid-isbn10? str)
-    (pregexp-match "^[0-9]{9}[0-9X]$" str))
+    (cond ((pregexp-match "^[0-9]{9}([0-9X])$" str)
+           => (lambda (m)
+                (let ((check (let ((c (car (string->list (cadr m)))))
+                               (case c
+                                 ((#\X) 10)
+                                 ((#\0) 11)
+                                 (else (char->digit c)))))
+                      (digits (map char->digit (string->list str))))
+                  (= check
+                     (- 11 (mod (apply + (map * digits '(10 9 8 7 6 5 4 3 2 0))) 11))))))
+          (else #f)))
 
   (define (valid-isbn? str)
     (cond ((valid-isbn13? str) 13)
--- tests/errata/isbn.scm
+++ tests/errata/isbn.scm
@@ -1,7 +1,59 @@
 #!/usr/bin/env ypsilon
 #!r6rs
 
-(import (errata isbn) (xunit))
+(import (rnrs) (errata isbn) (xunit))
+
+(define-syntax assert-valid-isbn
+  (syntax-rules (not)
+    ((_ proc)
+     #t)
+    ((_ proc (not s0) s1 ...)
+     (begin
+       (assert-boolean=? #f (proc s0))
+       (assert-valid-isbn proc s1 ...)))
+    ((_ proc s0 s1 ...)
+     (begin
+       (assert-boolean=? #t (proc s0))
+       (assert-valid-isbn proc s1 ...)))))
+
+(define-syntax assert-valid-isbn10
+  (syntax-rules ()
+    ((_ s0 ...)
+     (assert-valid-isbn valid-isbn10? s0 ...))))
+
+(define-syntax assert-valid-isbn13
+  (syntax-rules ()
+    ((_ s0 ...)
+     (assert-valid-isbn valid-isbn13? s0 ...))))
+
+(assert-valid-isbn10
+ "0000000000"
+ "1111111111"
+ "2222222222"
+ "3333333333"
+ "4444444444"
+ "5555555555"
+ "6666666666"
+ "7777777777"
+ "8888888888"
+ "9999999999"
+ "4894713195"
+ "4874084141"
+ )
+
+(assert-valid-isbn13
+ "0000000000000"
+ (not "1111111111111")
+ "2222222222222"
+ (not "3333333333333")
+ "4444444444444"
+ (not "5555555555555")
+ "6666666666666"
+ (not "7777777777777")
+ "8888888888888"
+ (not "9999999999999")
+ "9784894713192"
+ "9784874084144")
 
 (assert-boolean=? #f (valid-isbn? ""))
 (assert-= 10 (valid-isbn? "2222222222"))
