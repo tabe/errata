37b345d9a60e903e8fc3602293219a48a1933fad
Takeshi Abe <tabe@fixedpoint.jp>
2009-10-16 16:49:59 +0900

    tests for rss

--- tests/errata/rss/recent-reviews.scm
+++ tests/errata/rss/recent-reviews.scm
@@ -1,6 +1,21 @@
 #!/usr/bin/env ypsilon
 #!r6rs
 
-(import (errata rss recent-reviews) (xunit))
+(import (rnrs)
+        (errata configuration)
+        (errata rss recent-reviews)
+        (xunit))
+
+(skip-unless (and mysql-user mysql-password mysql-database)
+  (let ((set (feed-set mysql-user mysql-password mysql-database)))
+    (assert-list? set)
+    (for-each
+     (lambda (tuple)
+       (assert-list? (feed-entry tuple)))
+     set)
+    (for-each
+     (lambda (tuple)
+       (assert-list? (feed-item tuple)))
+     set)))
 
 (report)
--- tests/errata/rss/recent-revisions.scm
+++ tests/errata/rss/recent-revisions.scm
@@ -1,6 +1,21 @@
 #!/usr/bin/env ypsilon
 #!r6rs
 
-(import (errata rss recent-revisions) (xunit))
+(import (rnrs)
+        (errata configuration)
+        (errata rss recent-revisions)
+        (xunit))
+
+(skip-unless (and mysql-user mysql-password mysql-database)
+  (let ((set (feed-set mysql-user mysql-password mysql-database)))
+    (assert-list? set)
+    (for-each
+     (lambda (tuple)
+       (assert-list? (feed-entry tuple)))
+     set)
+    (for-each
+     (lambda (tuple)
+       (assert-list? (feed-item tuple)))
+     set)))
 
 (report)
